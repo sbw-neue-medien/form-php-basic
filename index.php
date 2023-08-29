847a4f507caff3e7768ca510f53d4d74da115b83
Johannes Kingma <j.kingma@sbw.edu>
2023-08-29 17:40:07 +0200

    initial

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+  <link rel="stylesheet" href="assets/main.css">
+</head>
+<html>
+<body>
+
+<form method='POST'>
+
+    <input type='radio' name='anrede' value='m'>Herr
+    <input type='radio' name='anrede' value='f'>Frau<br>
+    Vorname: <input type='text' name='vorname' value='' id='vorname'><br>
+    Nachname: <input type='text' name='nachname' value='' id='nachname'><br>
+    <input type='submit' value='ok'>
+
+</form>
+
+</body>
+</html>
